9acc0b81e573e92c43f707b68592296c5a2d70cc
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-06-23 19:18:15 +0200

    finish styles

--- src/components/4-transaction/Transactions.js
+++ src/components/4-transaction/Transactions.js
@@ -15,7 +15,7 @@ const TransactionHistory = ({ items }) => {
             <tbody>
                 {items.map(item => {
                     return (<tr key={item.id}>
-                        <td>{item.type}</td>
+                        <td className={css.text}>{item.type}</td>
                         <td>{item.amount}</td>
                         <td>{item.currency}</td>
                     </tr>)
